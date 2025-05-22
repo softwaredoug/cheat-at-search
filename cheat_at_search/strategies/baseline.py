df9f99a68848e4f941e1ec9913217e8a7a628d90
Doug Turnbull <softwaredoug@gmail.com>
2025-05-22 14:06:14 -0400

    Refactor to stratregies

--- cheat_at_search/strategies/baseline.py
+++ cheat_at_search/strategies/baseline.py
@@ -1,46 +1,37 @@
 from searcharray import SearchArray
 from cheat_at_search.tokenizers import snowball_tokenizer
 from cheat_at_search.wands_data import products, queries
+from cheat_at_search.strategies.strategy import SearchStrategy
 from cheat_at_search.eval import grade_results
 import numpy as np
 import pandas as pd
 
 
-class WandsSearch:
+class WandsSearch(SearchStrategy):
     def __init__(self, products):
+        super().__init__(products)
         self.index = products
         self.index['product_name_snowball'] = SearchArray.index(
             products['product_name'], snowball_tokenizer)
         self.index['product_description_snowball'] = SearchArray.index(
             products['product_description'], snowball_tokenizer)
 
-    def search(self, queries: pd.DataFrame, k=10) -> pd.DataFrame:
+    def search(self, query, k=10):
         """Dumb baseline lexical search"""
-        all_results = []
-        for _, query_row in queries.iterrows():
-            query = query_row['query']
-            query_id = query_row['query_id']
-            tokenized = snowball_tokenizer(query)
-            bm25_scores = np.zeros(len(self.index))
-            for token in tokenized:
-                bm25_scores += self.index['product_name_snowball'].array.score(token)
-                bm25_scores += self.index['product_description_snowball'].array.score(
-                    token)
-            top_k = np.argsort(-bm25_scores)[:k]
-            ranks = np.arange(len(top_k)) + 1
-            top_k_products = self.index.iloc[top_k].copy()
-            top_k_products['score'] = bm25_scores[top_k]
-            top_k_products['query'] = query
-            top_k_products['query_id'] = query_id
-            top_k_products['rank'] = ranks
-            all_results.append(top_k_products)
-
-        return pd.concat(all_results)
+        tokenized = snowball_tokenizer(query)
+        bm25_scores = np.zeros(len(self.index))
+        for token in tokenized:
+            bm25_scores += self.index['product_name_snowball'].array.score(token)
+            bm25_scores += self.index['product_description_snowball'].array.score(
+                token)
+        top_k = np.argsort(-bm25_scores)[:k]
+        scores = bm25_scores[top_k]
+        return top_k, scores
 
 
 if __name__ == '__main__':
     search = WandsSearch(products)
-    all_results = search.search(queries)
+    all_results = search.search_all(queries)
     graded = grade_results(all_results)
     dcgs = graded.groupby(['query', 'query_id'])['discounted_gain'].sum().sort_values(ascending=False)
     print(dcgs)
--- cheat_at_search/strategies/strategy.py
+++ cheat_at_search/strategies/strategy.py
@@ -1,8 +1,27 @@
+import pandas as pd
+import numpy as np
 
 
 class SearchStrategy:
 
-    def __init__(self, products):
-        self.products
+    def __init__(self, products, top_k=5):
+        self.products = products
+        self.top_k = top_k
 
-    def search(self, queries):
+    def search_all(self, queries, k=10):
+        all_results = []
+        for _, query_row in queries.iterrows():
+            top_k, scores = self.search(query_row['query'], k)
+            query_id = query_row['query_id']
+            ranks = np.arange(len(top_k)) + 1
+            top_k_products = self.index.iloc[top_k].copy()
+            top_k_products['score'] = scores
+            top_k_products['query'] = query_row['query']
+            top_k_products['query_id'] = query_id
+            top_k_products['rank'] = ranks
+            all_results.append(top_k_products)
+        return pd.concat(all_results)
+
+    def search(self, query):
+        # This method should be implemented by subclasses
+        raise NotImplementedError("Subclasses should implement this method.")
