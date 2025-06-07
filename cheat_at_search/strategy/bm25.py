9a6474857311bea46405f1bc029a33a62f54d018
Doug Turnbull <softwaredoug@gmail.com>
2025-06-07 08:26:47 -0400

    Add name and description boost to BM25 as a more consistent baseline

--- cheat_at_search/strategy/bm25.py
+++ cheat_at_search/strategy/bm25.py
@@ -5,9 +5,13 @@ import numpy as np
 
 
 class BM25Search(SearchStrategy):
-    def __init__(self, products):
+    def __init__(self, products,
+                 name_boost=5,
+                 description_boost=1):
         super().__init__(products)
         self.index = products
+        self.name_boost = name_boost
+        self.description_boost = description_boost
         self.index['product_name_snowball'] = SearchArray.index(
             products['product_name'], snowball_tokenizer)
         self.index['product_description_snowball'] = SearchArray.index(
@@ -18,9 +22,9 @@ class BM25Search(SearchStrategy):
         tokenized = snowball_tokenizer(query)
         bm25_scores = np.zeros(len(self.index))
         for token in tokenized:
-            bm25_scores += self.index['product_name_snowball'].array.score(token)
+            bm25_scores += self.index['product_name_snowball'].array.score(token) * self.name_boost
             bm25_scores += self.index['product_description_snowball'].array.score(
-                token)
+                token) * self.description_boost
         top_k = np.argsort(-bm25_scores)[:k]
         scores = bm25_scores[top_k]
         return top_k, scores
