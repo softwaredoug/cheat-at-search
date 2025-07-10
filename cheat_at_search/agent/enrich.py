ac8496f07a3de2262ce33873d0360333170cdaf7
Doug Turnbull <softwaredoug@gmail.com>
2025-07-10 16:23:36 -0400

    Also get class's properties

--- cheat_at_search/agent/enrich.py
+++ cheat_at_search/agent/enrich.py
@@ -397,6 +397,8 @@ class ProductEnricher:
         if attrs is None:  # Inferred from the BaseModel
             output_cls = enricher.output_cls
             attrs = output_cls.__fields__.keys()
+            # Get properties too
+            attrs = attrs.union(attr for attr in dir(output_cls) if isinstance(getattr(output_cls, attr), property))
         self.attrs = attrs
 
     def enrich_one(self, product_name: str, product_description: str, product_id: str):
