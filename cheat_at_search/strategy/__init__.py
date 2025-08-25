e73d334b0fda04121f2ade65ab2e026d3b509858
Doug Turnbull <softwaredoug@gmail.com>
2025-08-25 17:03:15 -0400

    Remove strategies

--- cheat_at_search/strategy/__init__.py
+++ cheat_at_search/strategy/__init__.py
@@ -1,6 +1,1 @@
 from .bm25 import BM25Search  # noqa: F401
-from .synonyms import SynonymSearch, AlternateLabelSearch, HypernymSearch, HyponymSearch  # noqa: F401
-from .corrected import SpellingCorrectedSearch, SpellingCorrectedSearch2, SpellingCorrectedSearch3  # noqa: F401
-from .rebuilt import StructuredSearch  # noqa: F401
-from .category import CategorySearch, CategorySearchDoubleCheck, CategorySearchOverfit, \
-    CategorySearchFillNans, CategorySearchFullyQualified, CategorySearchFullyQualifiedLabelAll  # noqa: F401
