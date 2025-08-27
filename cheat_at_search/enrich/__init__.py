8dbc836792fbc560dda773778d0625369d532505
Doug Turnbull <softwaredoug@gmail.com>
2025-08-27 14:57:14 -0400

    Back out instructor enrich client

--- cheat_at_search/enrich/__init__.py
+++ cheat_at_search/enrich/__init__.py
@@ -1,13 +1,12 @@
 from .enrich import AutoEnricher, ProductEnricher
 from .cached_enrich_client import CachedEnrichClient
 from .enrich_client import EnrichClient, DebugMetaData
-from .instructor_enrich_client import InstructorEnrichClient
+
 
 __all__ = [
     'AutoEnricher',
     'ProductEnricher',
     'CachedEnrichClient',
     'EnrichClient',
-    'DebugMetaData',
-    'InstructorEnrichClient'
+    'DebugMetaData'
 ]
