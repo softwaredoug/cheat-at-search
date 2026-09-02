a786c0609ce020e586dc7c0211e938daff91393b
Doug Turnbull <softwaredoug@gmail.com>
2026-09-02 08:49:22 -0400

    Batch all images

--- cheat_at_search/images.py
+++ cheat_at_search/images.py
@@ -57,14 +57,17 @@ class OpenAIImageGenerator:
                                file_path: str) -> None:
         """Generate batch requests for OpenAI image generation and write to a JSONL file."""
         products = products.iloc[start:start + num_products]
+        if len(products) == 0:
+            return None
         requests = products[["title", "description", "doc_id"]].apply(
             lambda row: self._batch_request(row["title"], row["description"], row["doc_id"]),
             axis=1,
         )
         # write as jsonl
         requests.to_json(file_path, orient="records", lines=True)
+        return file_path
 
-    def submit_batch(self, batch_file_path: str) -> None:
+    def submit_batch(self, batch_file_path: str, wait_for_processing=False) -> None:
         """Submit batch, block until done."""
         batch_file = self.openai.files.create(
             file=open(batch_file_path, "rb"),
@@ -89,17 +92,18 @@ class OpenAIImageGenerator:
             completion_window="24h",
         )
         # Loop until we confirm its in_progress
-        while True:
-            batch = self.openai.batches.retrieve(batch.id)
-            if batch.status in ["in_progress", "completed", "failed"]:
-                break
-            print(f"Batch {batch.id} status: {batch.status}. Waiting for processing...")
-            time.sleep(10)
-        if batch.status == "failed":
-            raise RuntimeError(f"Batch {batch.id} failed.")
+        if wait_for_processing:
+            while True:
+                batch = self.openai.batches.retrieve(batch.id)
+                if batch.status in ["in_progress", "completed", "failed"]:
+                    break
+                print(f"Batch {batch.id} status: {batch.status}. Waiting for processing...")
+                time.sleep(10)
+            if batch.status == "failed":
+                raise RuntimeError(f"Batch {batch.id} failed.")
         return batch.id
 
-    async def await_batch(self, batch_id: str, output_file_path: str, poll_every=60) -> None:
+    async def await_batch(self, batch_id: str, output_file_path: str, poll_every=80) -> None:
         print(f"Awaiting batch {batch_id} completion...")
         while True:
             batch = self.openai.batches.retrieve(batch_id)
@@ -110,13 +114,15 @@ class OpenAIImageGenerator:
         if batch.status == "failed":
             raise RuntimeError(f"Batch {batch_id} failed.")
         elif batch.status == "completed":
-            print(f"Batch {batch_id} completed successfully.")
+            print(f"Batch {output_file_path} completed successfully.")
 
         result = self.openai.files.content(batch.output_file_id)
         result.write_to_file(output_file_path)
 
     def process_output_batch(self, output_file_path: str, images_dir: str) -> pd.DataFrame:
         """Process the output batch file and save each with product id image."""
+
+        print(f"Processing output batch file: {output_file_path}")
         df = pd.read_json(output_file_path, lines=True)
         resp = pd.json_normalize(df["response"])
         columns_to_copy = ["request_id", "status_code", "body.data", "body.size"]
@@ -152,19 +158,49 @@ def files_to_gcs(images_dir: str) -> None:
     project = os.environ["GCLOUD_TRAINING_PROJECT"]
     bucket = "product-ai-images"
     prefix = "wands/images/"
+    upload_success_path = os.path.join(images_dir, "uploaded_images.txt")
     from google.cloud import storage
+    from google.cloud.storage import transfer_manager
+
+    past_upload_success = set()
+    try:
+        with open(upload_success_path, "r") as f:
+            for line in f:
+                past_upload_success.add(line.strip())
+    except FileNotFoundError:
+        pass
 
     client = storage.Client(project=project)
     bucket = client.bucket(bucket)
 
+    filenames = []
     for image_file in os.listdir(images_dir):
-        if image_file.endswith(".png"):
-            blob = bucket.blob(os.path.join(prefix, image_file))
-            blob.upload_from_filename(os.path.join(images_dir, image_file))
-            print(f"Uploaded {image_file} to {bucket}")
+        if image_file.endswith(".png") and image_file not in past_upload_success:
+            filenames.append(image_file)
+    results = transfer_manager.upload_files(
+        bucket,
+        filenames,
+        source_directory=images_dir,
+        blob_name_prefix=prefix,
+        max_workers=10,
+        worker_type=transfer_manager.THREAD,
+        content_type="image/png",
+    )
+
+    for filename, result in zip(filenames, results):
+        if isinstance(result, Exception):
+            print(f"Failed: {filename}: {result}")
+        else:
+            past_upload_success.add(filename)
+            print(f"Uploaded: {filename}")
+
+    print("Recording uploaded images...")
+    with open(upload_success_path, "w") as f:
+        for filename in past_upload_success:
+            f.write(f"{filename}\n")
 
 
-def main(images_dir, batch_size=10, num_batches=1, force=False):
+def process(images_dir, start_at=0, batch_size=10, end_batch=1):
     """Write images to images_dir for every wands product."""
     # Example usage
     batch_file_dir = ensure_data_subdir("image_batch_files")
@@ -182,20 +218,26 @@ def main(images_dir, batch_size=10, num_batches=1, force=False):
     generator = OpenAIImageGenerator(model="gpt-image-1")
 
     for batch_idx in range(0, len(corpus), batch_size):
+        if (batch_idx + batch_size) <= (start_at * batch_size):
+            print(f"Skipping batch {batch_idx} as it is before start_at {start_at}.")
+            continue
         print(f"Processing batch {batch_idx}...")
         print(f"Check out file: {out_file(batch_idx)}")
-        if not force and os.path.exists(out_file(batch_idx)):
-            print(f"Batch {batch_idx} already processed, skipping.")
+        if os.path.exists(out_file(batch_idx)):
+            print(f"Batch {batch_idx} already processed")
             processable_batches.append(out_file(batch_idx))
         else:
             print(f"Submitting batch {batch_idx}...")
-            generator.batch_requests_to_file(corpus,
-                                             start=batch_idx,
-                                             num_products=batch_size,
-                                             file_path=in_file(batch_idx))
+            submit_path = generator.batch_requests_to_file(corpus,
+                                                           start=batch_idx,
+                                                           num_products=batch_size,
+                                                           file_path=in_file(batch_idx))
+            if submit_path is None:
+                print(f"No products to process in batch {batch_idx}, skipping submission.")
+                continue
             batch_id = generator.submit_batch(in_file(batch_idx))
             submitted_batches.append((batch_id, batch_idx))
-        if (batch_idx + batch_size) >= (num_batches * batch_size):
+        if (batch_idx + batch_size) >= (end_batch * batch_size):
             break
 
     # Await all pending batches we don't have files for yet
@@ -205,7 +247,7 @@ def main(images_dir, batch_size=10, num_batches=1, force=False):
             tasks = {}
             for idx, (batch_id, batch_idx) in enumerate(submitted_batches):
                 op_file = out_file(batch_idx)
-                print(f"Output saved to {op_file}")
+                print(f"Output to be saved to {op_file}")
                 task = asyncio.create_task(generator.await_batch(batch_id, op_file))
                 tasks[task] = (batch_id, batch_idx, op_file)
 
@@ -226,9 +268,20 @@ def main(images_dir, batch_size=10, num_batches=1, force=False):
         generator.process_output_batch(processed_path, images_dir)
         print(f"{processed_path} images written to {images_dir}")
 
-    # files_to_gcs(images_dir)
+
+def main():
+    images_dir = ensure_data_subdir("images")
+    # end_batch = 275
+    # start_at = 265
+    # while start_at < 500:
+    #     process(images_dir,
+    #             start_at=start_at,
+    #             batch_size=100,
+    #             end_batch=end_batch)
+    #     start_at += 10
+    #     end_batch += 10
+    files_to_gcs(images_dir)
 
 
 if __name__ == "__main__":
-    images_dir = ensure_data_subdir("images")
-    main(images_dir, batch_size=100, num_batches=100)
+    main()
